8bab7690c2eed0c099963abd7234d9878ee9d523
Deepakvijayan07 <deepakvijayan31@gmail.com>
2023-12-22 12:59:56 +0530

    Auto-generated commit [ci skip]

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('.mask1').scroll(function () {
+        var scroll = $('.mask1').scrollTop();
+        if (scroll >= 80) {
+          $(".mask1 .opacity-box").hide();
+        }
+        else {
+          $(".mask1 .opacity-box").show();
+        }
+    });
+    $('.mask2').scroll(function () {
+        var scroll = $('.mask2').scrollTop();
+        if (scroll >= 80) {
+          $(".mask2 .opacity-box").hide();
+        }
+        else {
+          $(".mask2 .opacity-box").show();
+        }
+    });
+});
+
+
+$(document).ready(function () {
+  $( ".animation-area li" ).draggable();
+});
+
